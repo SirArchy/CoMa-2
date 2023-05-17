a4c17cbc00104d19ddf08e4172d74497344dfe41
Fabian Ebert <fabian.ebert@online.de>
2023-05-17 12:14:45 +0200

    added PA 4

--- WIP/CoMa_Proggen_HA4.jl
+++ WIP/CoMa_Proggen_HA4.jl
@@ -1,32 +1,77 @@
-mutable struct Node
-    successors::Vector{Node}
-    name::String
-    color::Symbol
+# Definiere den Type Alias TupleSet
+const TupleSet = Vector{Tuple{Int, Int}}
 
-    function Node(name::String)
-        new(Vector{Node}(), name, :white)
-    end
+# Definiere den Type Partition
+struct Partition
+    Sets::Vector{TupleSet}
 end
 
-function dfs(node::Node, sorted::Vector{Node})
-    if node.color == :gray
-        error("Der Graph enthält einen Kreis!")
-    elseif node.color == :white
-        node.color = :gray
-        for successor in node.successors
-            dfs(successor, sorted)
-        end
-        node.color = :black
-        push!(sorted, node)
-    end
+# Funktion a) Partition(V::TupleSet)::Partition
+function Partition(V::TupleSet)::Partition
+    return Partition([V])
 end
 
-function top_sort(G::Vector{Node})
-    sorted = Vector{Node}()
-    for node in G
-        if node.color == :white
-            dfs(node, sorted)
+# Funktion c) MakeSet(P::Partition, (x,y)::Tuple{Int,Int})
+function MakeSet(P::Partition, tuple::Tuple{Int,Int})
+    for set in P.Sets
+        if tuple in set
+            return
         end
     end
-    return reverse(sorted)
+    push!(P.Sets, [tuple])
 end
+
+# Funktion d) FindSet(P::Partition, (x,y)::Tuple{Int,Int})
+function FindSet(P::Partition, tuple::Tuple{Int,Int})
+    for set in P.Sets
+        if tuple in set
+            return sort(set)[1]
+        end
+    end
+    return -1
+end
+
+# Funktion e) union!(P::Partition, (x1,y1)::Tuple{Int,Int}, (x2,y2)::Tuple{Int,Int})
+function union!(P::Partition, tuple1::Tuple{Int,Int}, tuple2::Tuple{Int,Int})
+    set1 = FindSet(P, tuple1)
+    set2 = FindSet(P, tuple2)
+    
+    if set1 != -1 && set2 != -1
+        sets_to_remove = []
+        for (i, set) in enumerate(P.Sets)
+            if set == set1 || set == set2
+                push!(sets_to_remove, i)
+            end
+        end
+        
+        new_set = sort([tuple1, tuple2])
+        for index in reverse(sets_to_remove)
+            splice!(P.Sets, index)
+        end
+        push!(P.Sets, new_set)
+    end
+end
+
+S = TupleSet([(0,3),(0,1),(1,3),(1,0)]);
+P = Partition(S);
+
+println(union!(P,(1,3),(0,1)).Sets)
+# 3−elemen t Vec tor {Vec tor {Tuple { In t64 , I n t 6 4 }}} :
+# [ ( 0 , 3 ) ]
+# [ ( 1 , 0 ) ]
+# [ ( 0 , 1 ) , ( 1 , 3 ) ]
+println(union!(P,(0,1),(0,3)).Sets)
+# 2−elemen t Vec tor {Vec tor {Tuple { In t64 , I n t 6 4 }}} :
+# [ ( 1 , 0 ) ]
+# [ ( 0 , 1 ) , ( 0 , 3 ) , ( 1 , 3 ) ]
+println(FindSet(P,(0,3)))
+# ( 0 , 1 )
+println(MakeSet(P,(300,1)).Sets)
+# 3−elemen t Vec tor {Vec tor {Tuple { In t64 , I n t 6 4 }}} :
+# [ ( 1 , 0 ) ]
+# [ ( 0 , 1 ) , ( 0 , 3 ) , ( 1 , 3 ) ]
+# [ ( 3 0 0 , 1 ) ]
+println(union!(P,(300,1),(0,1)).Sets)
+# 2−elemen t Vec tor {Vec tor {Tuple { In t64 , I n t 6 4 }}} :
+# [ ( 1 , 0 ) ]
+# [ ( 0 , 1 ) , ( 0 , 3 ) , ( 1 , 3 ) , ( 3 0 0 , 1 ) ]
