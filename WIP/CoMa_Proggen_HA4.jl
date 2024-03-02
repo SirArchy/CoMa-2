dd7133d1f99383f5a639773cebbd2fa3a2778dcd
Fabian Ebert <fabian.ebert@online.de>
2024-03-02 20:41:41 +0100

    draft pa4

--- WIP/CoMa_Proggen_HA4.jl
+++ WIP/CoMa_Proggen_HA4.jl
@@ -1,67 +1,85 @@
-# Type TupleSet 
-struct TupleSet
-    const TupleSet = Vector{Tuple{Int, Int}}
-    function TupleSet(name::String)
-        new(TupleSet(V::Vector{Tuple{Int, Int}}) = sort(V))
+# Definieren des Typs Set
+struct Set
+    _elements::Vector{Tuple{Int, Int}}
+end
+
+# Initialisieren eines Sets mit einer gegebenen Liste von Tupeln
+function Set(V::Vector{Tuple{Int, Int}})
+    # Prüfen, ob es doppelte Elemente gibt
+    length(unique(V)) != length(V) && throw("invalid operation")
+    return Set(sort(V))
+end
+
+# Gibt die Elemente des Sets zurück
+elements(s::Set) = s._elements
+
+# Definieren des Typs Partition
+struct Partition
+    Sets::Vector{Set}
+end
+
+# Initialisiert eine Partition von V in einelementige Mengen
+function Partition(V::Vector{Tuple{Int, Int}})
+    # Prüfen, ob es doppelte Elemente gibt
+    length(unique(V)) != length(V) && throw("invalid operation")
+    sets = [Set([(x, y)]) for (x, y) in V]
+    return Partition(sets)
+end
+
+# String-Repräsentation eines Partition-Objekts
+Base.show(io::IO, P::Partition) = print(io, [elements(s) for s in P.Sets])
+
+# Fügt der Liste Sets ein Set-Objekt hinzu, das mit dem Tupel (x, y) initialisiert wird
+function MakeSet!(P::Partition, xy::Tuple{Int, Int})
+    for set in P.Sets
+        xy ∈ elements(set) && throw("invalid operation")
+    end
+    push!(P.Sets, Set([xy]))
+end
+
+# Findet das Set, das das Tupel (x,y) enthält und gibt das Repräsentanten-Tupel zurück
+function FindSet(P::Partition, xy::Tuple{Int, Int})
+    for set in P.Sets
+        if xy ∈ elements(set)
+            return elements(set)[1]
+        end
+    end
+    throw("invalid operation")
+end
+
+# Bildet die Vereinigung zweier Sets in der Partition, deren Repräsentanten (x1,y1) bzw. (x2,y2) sind
+function Union!(P::Partition, xy1::Tuple{Int, Int}, xy2::Tuple{Int, Int})
+    index1, index2 = nothing, nothing 
+    for i in 1:length(P.Sets)
+        if elements(P.Sets[i])[1] == xy1
+            index1 = i
+        elseif elements(P.Sets[i])[1] == xy2
+            index2 = i
+        end
+    end
+    
+    if index1 !== nothing && index2 !== nothing
+        # Kombinieren der Elemente und Entfernen der alten Sets
+        new_set_elements = unique(vcat(elements(P.Sets[index1]), elements(P.Sets[index2])))
+        new_set = Set(sort(new_set_elements))
+        deleteat!(P.Sets, [index1, index2])
+        push!(P.Sets, new_set)
+    else
+        throw("invalid operation")
     end
 end
 
-# Type Partition
-struct Partition
-    Sets::Vector{TupleSet}
-end
 
-# Function Partition
-function Partition(V::TupleSet)
-    return new(Partition(V))
-end
-
-# Function MakeSet
-function MakeSet(P::Partition, (x,y)::Tuple{Int,Int}) 
-    return
-end
-
-# Function FindSet
-function FindSet(P::Partition, (x,y)::Tuple{Int,Int}) 
-    return
-end
-
-# Function union!
-function union!(P::Partition, (x1,y1)::TupleInt,Int, (x2,y2)::TupleInt,Int) 
-    return
-end
-
-
-S = TupleSet([(0,3),(0,1),(1,3),(1,0)])
-P = Partition(S)
-#=
-println(union!(P,(1,3),(0,1)).Set)
-# 3−element Vector {Vector {Tuple { Int64 , Int64 }}} :
-# [(0,3)]
-# [(1,0)]
-# [(0,1),(1,3)]
-println(union!(P,(0,1),(0,3)).Set)
-# 2−element Vector {Vector {Tuple { Int64 , Int64 }}} :
-# [(1,0)]
-# [(0,1),(0,3),(1,3)]
-println(FindSet(P,(0,3)))
-# (0,1)
-println(MakeSet(P,(300,1)).Set)
-# 3−element Vector {Vector {Tuple { Int64 , Int64 }}} :
-# [(1,0)]
-# [(0,1),(0,3),(1,3)]
-# [(300,1)]
-println(union!(P,(300,1),(0,1)).Set)
-# 2−element Vector {Vector {Tuple { Int64 , Int64 }}} :
-# [(1,0)]
-# [(0,1),(0,3),(1,3),(300,1)]
-#=
-=#
-
-
-#= Input: [(1,3),(2,1)]
-Erwarteter Output: [(1, 3), (2, 1), (457, 23), (457, 501), (342, 11), (110, 1), (110, 2)]
-All Good
-(110, 1)
-=#
-=#
+S = Partition([(0, 3), (0, 1), (1, 3), (1, 0)])
+Union!(S, (1, 3), (0, 1))
+Union!(S, (0, 3), (0, 1))
+println(S)
+println(FindSet(S, (1, 3)))
+MakeSet!(S, (300, 1))
+Union!(S, (300, 1), (0, 1))
+println(S)
+println(FindSet(S, (300, 1)))
+MakeSet!(S, (0, 0))
+Union!(S, (0, 0), (0, 1))
+println(FindSet(S, (300, 1)))
+println(S)
