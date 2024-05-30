d373eeac3002638d99a89859637294e241345a0e
SirArchy <wnbemail123@googlemail.com>
2024-05-30 20:00:16 +0200

    finished task 5 and added task 6

--- Julia_24/CoMa_Proggen_HA6.jl
+++ Julia_24/CoMa_Proggen_HA6.jl
@@ -1,155 +1,101 @@
-struct IntVektor
-    x::Int
-    y::Int
-    z::Int
+struct FactorTree
+    value::Int
+    left::Union{Nothing, FactorTree}
+    right::Union{Nothing, FactorTree}
 end
 
-Base.:+(v1::IntVektor, v2::IntVektor) = IntVektor(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z)
-
-Base.:*(v::IntVektor, scalar::Int) = IntVektor(v.x * scalar, v.y * scalar, v.z * scalar)
-Base.:*(scalar::Int, v::IntVektor) = v * scalar
-
-function copy(v::IntVektor)
-    return IntVektor(v.x, v.y, v.z)
-end
-
-Base.show(io::IO, v::IntVektor) = print(io, "(", v.x, ", ", v.y, ", ", v.z, ")")
-
-mutable struct Teilgitter <: IntVektor
-    koordinate_1::Int
-    koordinate_2::Int
-end
-
-function Teilgitter(x::Int, y::Int, z::Int)
-    b1 = IntVektor(2, 1, 15)
-    b2 = IntVektor(1, 0, 5)
-    
-    k1, k2 = calculate_coordinates(x, y, z, b1, b2)
-    
-    if k1 === nothing || k2 === nothing
-        error("Vektor liegt nicht im Teilgitter.")
-    end
-    
-    new_teilgitter = new(x, y, z)
-    new_teilgitter.koordinate_1 = k1
-    new_teilgitter.koordinate_2 = k2
-    return new_teilgitter
-end
-
-function calculate_coordinates(x, y, z, b1, b2)
-    if z % b2.z !== 0
-        return nothing, nothing
-    end
-    
-    k2 = div(z, b2.z)
-    adjusted_x = x - k2
-    adjusted_y = y
-    
-    if adjusted_x % b1.x !== 0 || adjusted_y % b1.y !== 0
-        return nothing, nothing
-    end
-    
-    k1 = div(adjusted_x, b1.x)
-    
-    if b1.z * k1 + b2.z * k2 == z
-        return k1, k2
+function FactorTree(v::Int)
+    if isprime(v)
+        return FactorTree(v, nothing, nothing)
     else
-        return nothing, nothing
+        a, b = best_factor_pair(v)
+        return FactorTree(v, FactorTree(a), FactorTree(b))
     end
 end
 
-Base.show(io::IO, g::Teilgitter) = print(io, "(", g.x, ", ", g.y, ", ", g.z, "); Koordinate 1: ", g.koordinate_1, ", Koordinate 2: ", g.koordinate_2)
+function isprime(n::Int)
+    if n <= 1
+        return false
+    elseif n <= 3
+        return true
+    elseif n % 2 == 0 || n % 3 == 0
+        return false
+    end
+    i = 5
+    while i^2 <= n
+        if n % i == 0 || n % (i + 2) == 0
+            return false
+        end
+        i += 6
+    end
+    return true
+end
 
-Base.:+(g1::Teilgitter, g2::Teilgitter) = Teilgitter(g1.x + g2.x, g1.y + g2.y, g1.z + g2.z)
+function best_factor_pair(n::Int)
+    best_a, best_b = 1, n
+    for i in 2:floor(Int, sqrt(n))
+        if n % i == 0
+            a, b = i, div(n, i)
+            if b - a < best_b - best_a
+                best_a, best_b = a, b
+            end
+        end
+    end
+    return best_a, best_b
+end
 
-Base.:*(g::Teilgitter, scalar::Int) = Teilgitter(scalar * g.x, scalar * g.y, scalar * g.z)
-Base.:*(scalar::Int, g::Teilgitter) = g * scalar
+function getFactors(t::FactorTree)::Dict{Int, Int}
+    factors = Dict{Int, Int}()
+    function _getFactors(t::FactorTree)
+        if isprime(t.value)
+            factors[t.value] = get(factors, t.value, 0) + 1
+        else
+            _getFactors(t.left)
+            _getFactors(t.right)
+        end
+    end
+    _getFactors(t)
+    return factors
+end
 
-#=
-# Example usage
-A = Teilgitter(10, 3, 23)
-println(A)
+function getShape(t::FactorTree)::String
+    if isprime(t.value)
+        return "p"
+    elseif isprime(t.left.value) && isprime(t.right.value)
+        return "p2"
+    else
+        return "f(" * getShape(t.left) * "|" * getShape(t.right) * ")"
+    end
+end
 
-B = Teilgitter(14, 4, 34)
-println(B)
+function compareShape(t1::FactorTree, t2::FactorTree)::Bool
+    return getShape(t1) == getShape(t2)
+end
 
-C = A + B
-println(C)
-
-D = 3 * A
-println(D)
-
-E = -3 * A
-println(E)
-
-F = B * 7
-println(F)
-
-# The product A * B doesn't make sense in this context because we don't have a definition for multiplying two Teilgitter objects. We would need additional information about what this operation represents.
-
-G = copy(A)
-println(G)
-
-H = Teilgitter(9, 5, 25)
-println(H)
-
-
-# Beispielaufrufe
-
-str = "58(49(89,),93(80,))"
-tree = fromString(str)
-println(tree)
-println(getKeyList(tree))
-println(find(tree, 5))
-println(min(tree))
-
-str = "4(1,5)"
-tree = fromString(str)
-println(tree)
-println(getKeyList(tree))
-println(find(tree, 5))
-println(min(tree))
-
-str = "3(2(1,2),5(4,))"
-tree = fromString(str)
-println(tree)
-println(getKeyList(tree))
-println(find(tree, 5))
-println(min(tree))
-
-str = "9(,97)"
-tree = fromString(str)
-println(tree)
-println(getKeyList(tree))
-println(find(tree, 5))
-println(min(tree))
-
-str = "13(,58(52(,57(,57(57(,57(57(57,),57)),))),71))"
-tree = fromString(str)
-println(tree)
-println(getKeyList(tree))
-println(find(tree, 5))
-println(min(tree))
-#=
-Ausgabe: 
-Node(4, Node(1, nothing, nothing, Node(4, nothing, nothing, nothing)), Node(5, nothing, nothing, Node(4, nothing, nothing, nothing)), nothing)
-Node(3, Node(2, Node(1, nothing, nothing, Node(2, Node(1, nothing, nothing, nothing), Node(2, nothing, nothing, nothing), nothing)), Node(2, Node(1, nothing, nothing, Node(2, Node(1, nothing, nothing, nothing), Node(2, nothing, nothing, nothing), nothing)), Node(2, nothing, nothing, nothing)), nothing)), Node(5, Node(4, nothing, nothing, Node(5, Node(4, nothing, nothing, nothing), nothing, nothing)), nothing, Node(3, Node(2, Node(1, nothing, nothing, Node(2, Node(1, nothing, nothing, nothing), Node(2, nothing, nothing, nothing), nothing)), Node(2, nothing, nothing, nothing)), Node(5, Node(4, nothing, nothing, Node(5, Node(4, nothing, nothing, nothing), nothing, nothing)), nothing, nothing)), nothing)), nothing))
-=#
-
+function computeShapes(n::Int)::Dict{String, Vector{Int}}
+    shapes_dict = Dict{String, Vector{Int}}()
+    for i in 1:n
+        if i == 1
+            shape = "p"
+        else
+            t = FactorTree(i)
+            shape = getShape(t)
+        end
+        if haskey(shapes_dict, shape)
+            push!(shapes_dict[shape], i)
+        else
+            shapes_dict[shape] = [i]
+        end
+    end
+    return shapes_dict
+end
 
 
-# Beispielaufrufe
-tree1 = "13(,58(52(,57(,57(57(,57(57(57,),57)),))),71))"
-tree1 = fromString(tree1)
-min1 = min(tree1)
-println(min1)
-println(getKeyList(tree1))
-println(find(tree1, min1))
-# Erwarteter Output: 
-# fromString() korrekt: true
-# min: 13
-# KeyList: [13, 52, 57, 57, 57, 57, 57, 57, 57, 58, 71]
-# find the minimum: 13(,58(52(,57(,57(57(,57(57(57,),57)),))),71))
-# unable to find 0: true
-# n_nodes: 11
-=#
+# Beispiel
+#t = FactorTree(20)
+#getFactors(t)                # Dict{Int, Int64} with 2 entries: 5 => 1, 2 => 2
+#getShape(t)                  # "f(p2|p)"
+#t2 = FactorTree(945)
+#t3 = FactorTree(72)
+#compareShape(t2, t3)         # true
+#computeShapes(10)            # Dict{String, Vector{Int64}}
--- Julia_24/test.jl
+++ Julia_24/test.jl
@@ -1,16 +1,14 @@
-# Definieren des Node-structs
-struct Node
+mutable struct Node
     value::Union{Char,Nothing}
     freq::Int
     left::Union{Node,Nothing}
     right::Union{Node,Nothing}
 end
 
-# Implementierung der Funktion, um die Häufigkeiten zu ermitteln
 function getFrequencies(text::String)::Dict{Char,Int}
     freqs = Dict{Char,Int}()
     for char in text
-        if haskey(freqs, char)
+        if char in keys(freqs)
             freqs[char] += 1
         else
             freqs[char] = 1
@@ -19,90 +17,96 @@ function getFrequencies(text::String)::Dict{Char,Int}
     return freqs
 end
 
-# Implementierung der Funktion findLowestTwo
 function findLowestTwo(q1::Vector{Node}, q2::Vector{Node})::Tuple{Node, Node}
-    node1 = popfirst!(q1)
-    node2 = isempty(q2) ? popfirst!(q1) : popfirst!(q2)
-    
-    if isempty(q1) && !isempty(q2) && node2.freq < node1.freq
-        return (node2, node1)
-    end
-    return (node1, node2)
+    combined = vcat(q1, q2)
+    sort!(combined, by = x -> x.freq)
+    return (combined[1], combined[2])
 end
 
-# Implementierung der Funktion, um den Huffman-Baum zu erzeugen
 function huffman_tree(freqs::Dict{Char,Int})::Node
-    q1 = [Node(char, freq, nothing, nothing) for (char, freq) in sort(collect(freqs), by=x->x[2])]
-    q2 = Vector{Node}()
+    q1 = [Node(char, freqs[char], nothing, nothing) for char in keys(freqs)]
+    sort!(q1, by = x -> x.freq)
+    q2 = Node[]
+    
     while length(q1) + length(q2) > 1
-        node1, node2 = findLowestTwo(q1, q2)
-        combined_node = Node(nothing, node1.freq + node2.freq, node1, node2)
-        push!(q2, combined_node)
-    end
-    return isempty(q1) ? popfirst!(q2) : popfirst!(q1)
-end
-
-# Helper function to recursively build Huffman Code map
-function build_huffman_code(node::Node, code::String, code_map::Dict{Char, String})
-    if node.value !== nothing
-        code_map[node.value] = code
-    else
-        if node.left !== nothing
-            build_huffman_code(node.left, code * "0", code_map)
+        (a, b) = findLowestTwo(q1, q2)
+        
+        if a in q1
+            deleteat!(q1, findfirst(isequal(a), q1))
+        else
+            deleteat!(q2, findfirst(isequal(a), q2))
         end
-        if node.right !== nothing
-            build_huffman_code(node.right, code * "1", code_map)
+        
+        if b in q1
+            deleteat!(q1, findfirst(isequal(b), q1))
+        else
+            deleteat!(q2, findfirst(isequal(b), q2))
+        end
+
+        new_node = Node(nothing, a.freq + b.freq, a, b)
+        push!(q2, new_node)
+    end
+    
+    return q2[1]
+end
+
+function huffman_code(tree::Node)::Dict{Char, String}
+    codes = Dict{Char, String}()
+
+    function traverse(node::Node, path::String)
+        if node.value !== nothing
+            codes[node.value] = path
+        else
+            if node.left !== nothing
+                traverse(node.left, path * "0")
+            end
+            if node.right !== nothing
+                traverse(node.right, path * "1")
+            end
         end
     end
+    
+    traverse(tree, "")
+    return codes
 end
 
-# Hilfsfunktion, um das Huffman-Codebuch zu erstellen
-function huffman_codebook(tree::Node)::Dict{Char, String}
-    code_map = Dict{Char, String}()
-    build_huffman_code(tree, "", code_map)
-    return code_map
-end
-
-# Implementierung der Funktion Encode
 function Encode(text::String, code::Dict{Char, String})::String
-    encoded = String[]
+    encoded = ""
     for char in text
-        push!(encoded, code[char])
+        encoded *= code[char]
     end
-    return join(encoded)
+    return encoded
 end
 
-# Implementierung der Funktion Decode
 function Decode(encoded::String, tree::Node)::String
-    decoded_chars = Char[] 
-    current = tree        
+    decoded = ""
+    node = tree
     for bit in encoded
         if bit == '0'
-            current = current.left
-        else
-            current = current.right
+            node = node.left
+        elseif bit == '1'
+            node = node.right
         end
-        if current.value !== nothing
-            push!(decoded_chars, current.value) 
-            current = tree  
+        if node.value !== nothing
+            decoded *= node.value
+            node = tree
         end
     end
-    return join(decoded_chars) 
+    return decoded
 end
+
 
 # Beispielaufrufe
 #Example = "Wenn der Physiker nicht weiter weiß, gründet er ein Arbeitskreis"
 #freqs = getFrequencies(Example)
 #hufftree = huffman_tree(freqs)
 #huffcode = huffman_codebook(hufftree)
-#x = [Node(nothing,1,nothing,nothing), Node(nothing,4,nothing,nothing)]
-#y = [Node(nothing,2,nothing,nothing), Node(nothing,3,nothing,nothing)]
-#findLowestTwo(x, y)
-#x
 #encoded = Encode(Example, huffcode)
-## Zum Dekodieren wird die Wurzel des Huffman-Baums und der encodierte String benötigt
 #decoded = Decode(encoded, hufftree)
-#
-#println("Original: ", Example)
-#println("Encoded: ", encoded)
-#println("Decoded: ", decoded)
+
+#Example = "Warum sind da Limetten bei den Tomaten?"
+#freqs = getFrequencies(Example)
+#hufftree = huffman_tree(freqs)
+#huffcode = huffman_codebook(hufftree)
+#encoded = Encode(Example, huffcode)
+#decoded = Decode(encoded, hufftree)
